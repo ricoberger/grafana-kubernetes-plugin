dae177a74847d94048e13bbf26545d68f7f630b4
ricoberger <mail@ricoberger.de>
2025-10-17 12:02:16 +0200

    Add More Variable Types
    Until now only resource ids and namespaces could be used as variables.
    Now it is also possible to use resources and containers as variables.
    
    For this we also improved the default queries for all query types.

--- src/datasource/components/VariableQueryEditor/VaraibleQueryEditor.tsx
+++ src/datasource/components/VariableQueryEditor/VaraibleQueryEditor.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import { QueryEditorProps } from '@grafana/data';
-import { InlineField, RadioButtonGroup } from '@grafana/ui';
+import {
+  InlineFieldRow,
+  InlineField,
+  Combobox,
+  ComboboxOption,
+} from '@grafana/ui';
 
 import { DataSource } from '../../datasource';
-import { Query, QueryType } from '../../types/query';
+import { DEFAULT_QUERIES, Query, QueryType } from '../../types/query';
 import { DataSourceOptions } from '../../types/settings';
+import { KubernetesResources } from './KubernetesResources';
+import { KubernetesContainers } from './KubernetesContainers';
 
 interface Props
   extends QueryEditorProps<DataSource, any, DataSourceOptions, Query> { }
@@ -14,25 +21,50 @@ export function VariableQueryEditor(props: Props) {
   const { query, onChange } = props;
 
   /**
-   * Handle "queryType" field change. We support "kubernetes-resourceids" and
-   * "kubernetes-namespaces" queries for variables.
+   * Handle "queryType" field change. If the "queryType" is changed we also set
+   * the default queriy for that query tpe.
    */
-  const onTypeChange = (value: QueryType) => {
-    onChange({ ...query, queryType: value });
+  const onTypeChange = (option: ComboboxOption<QueryType>) => {
+    onChange({
+      ...query,
+      ...DEFAULT_QUERIES[option.value],
+      queryType: option.value,
+    });
   };
 
   return (
     <>
-      <InlineField label="Type" labelWidth={20}>
-        <RadioButtonGroup<QueryType>
-          options={[
-            { label: 'Resources', value: 'kubernetes-resourceids' },
-            { label: 'Namespaces', value: 'kubernetes-namespaces' },
-          ]}
-          value={query.queryType}
-          onChange={onTypeChange}
-        />
-      </InlineField>
+      <InlineFieldRow>
+        <InlineField label="Type">
+          <Combobox<QueryType>
+            value={query.queryType}
+            options={[
+              {
+                label: 'Kubernetes: Resources IDs',
+                value: 'kubernetes-resourceids',
+              },
+              {
+                label: 'Kubernetes: Namespaces',
+                value: 'kubernetes-namespaces',
+              },
+              {
+                label: 'Kubernetes: Containers',
+                value: 'kubernetes-containers',
+              },
+              { label: 'Kubernetes: Resources', value: 'kubernetes-resources' },
+            ]}
+            onChange={onTypeChange}
+          />
+        </InlineField>
+      </InlineFieldRow>
+
+      {query.queryType === 'kubernetes-containers' && (
+        <KubernetesContainers {...props} />
+      )}
+
+      {query.queryType === 'kubernetes-resources' && (
+        <KubernetesResources {...props} />
+      )}
     </>
   );
 }
