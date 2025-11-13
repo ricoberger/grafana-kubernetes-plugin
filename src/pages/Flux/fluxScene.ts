382496ce928efa0cd4b5a6e361bbfea33f793783
ricoberger <mail@ricoberger.de>
2025-11-13 08:24:54 +0100

    Use Default Queries in Pages
    We are now using the default queries from the explore view in the
    Kubernetes resources, Helm and Flux pages. This ensures consistency
    across different sections of the plugin and leverages the existing query
    configurations.

--- src/pages/Flux/fluxScene.ts
+++ src/pages/Flux/fluxScene.ts
@@ -15,6 +15,7 @@ import {
 import { VariableSort } from '@grafana/data';
 
 import datasourcePluginJson from '../../datasource/plugin.json';
+import { DEFAULT_QUERIES } from '../../datasource/types/query';
 
 export function fluxScene() {
   const datasourceVariable = new DataSourceVariable({
@@ -26,9 +27,9 @@ export function fluxScene() {
   const resourceVariable = new CustomVariable({
     name: 'resource',
     label: 'Resource',
-    value: 'TODO',
     query:
       'Buckets : buckets.source.toolkit.fluxcd.io, Git Repositories : gitrepositories.source.toolkit.fluxcd.io, Helm Charts : helmcharts.source.toolkit.fluxcd.io, Helm Repositories : helmrepositories.source.toolkit.fluxcd.io, OCI Repositories : ocirepositories.source.toolkit.fluxcd.io, Kustomizations : kustomizations.kustomize.toolkit.fluxcd.io, Helm Releases : helmreleases.helm.toolkit.fluxcd.io, Image Policies : imagepolicies.image.toolkit.fluxcd.io, Image Repositories : imagerepositories.image.toolkit.fluxcd.io, Image Update Automations : imageupdateautomations.image.toolkit.fluxcd.io, Alerts : alerts.notification.toolkit.fluxcd.io, Providers : providers.notification.toolkit.fluxcd.io, Receivers : receivers.notification.toolkit.fluxcd.io,',
+    value: DEFAULT_QUERIES['flux-resources'].resource,
   });
 
   const namespaceVariable = new QueryVariable({
@@ -43,6 +44,7 @@ export function fluxScene() {
       queryType: 'kubernetes-namespaces',
     },
     sort: VariableSort.alphabeticalCaseInsensitiveAsc,
+    value: DEFAULT_QUERIES['flux-resources'].namespace,
   });
 
   const queryRunner = new SceneQueryRunner({
