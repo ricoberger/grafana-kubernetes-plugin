0c9b882e6daa8ff6c57e06eff54f3d733ffb7348
Rico Berger <ricoberger@users.noreply.github.com>
2025-11-14 14:52:37 +0100

    Add Action to Approve and Deny CSR (#42)
    This commit adds two new actions for CertificateSigningRequests (CSRs),
    so that CSRs can be approved or denied directly from the UI.

---
+++ src/datasource/components/Kubernetes/CSRApproveAction.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import {
+  ConfirmModal,
+  LoadingPlaceholder,
+  Stack,
+  useStyles2,
+} from '@grafana/ui';
+import { AppEvents, GrafanaTheme2 } from '@grafana/data';
+import { getAppEvents } from '@grafana/runtime';
+import { css } from '@emotion/css';
+
+import { getResource } from '../../../utils/utils.resource';
+
+interface Props {
+  datasource?: string;
+  resource?: string;
+  namespace?: string;
+  name?: string;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function CSRApproveAction(props: Props) {
+  const styles = useStyles2(getStyles);
+  const [isLoading, setIsLoading] = useState(false);
+
+  /**
+   * To create a CertificateSigningRequest follow the steps mentioned in
+   * https://kubernetes.io/docs/tasks/tls/certificate-issue-client-csr/
+   */
+  const onConfirm = async () => {
+    try {
+      setIsLoading(true);
+
+      const resource = await getResource(props.datasource, props.resource);
+
+      const response = await fetch(
+        `/api/datasources/uid/${props.datasource}/resources/kubernetes/proxy${resource.path}/${resource.resource}/${props.name}/approval?fieldManager=grafana-kubernetes-plugin`,
+        {
+          method: 'patch',
+          headers: {
+            Accept: 'application/json, */*',
+            'Content-Type': 'application/merge-patch+json',
+          },
+          body: JSON.stringify({
+            status: {
+              conditions: [
+                {
+                  type: 'Approved',
+                  status: 'True',
+                  reason: 'GrafanaKubernetesPluginApprove',
+                  message:
+                    'This CSR was approved by Grafana Kubernetes Plugin certificate approve.',
+                  lastUpdateTime: new Date().toJSON(),
+                  lastTransitionTime: null,
+                },
+              ],
+            },
+          }),
+        },
+      );
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const appEvents = getAppEvents();
+      appEvents.publish({
+        type: AppEvents.alertSuccess.name,
+        payload: [`${props.name} was approved`],
+      });
+    } catch (_) {
+      const appEvents = getAppEvents();
+      appEvents.publish({
+        type: AppEvents.alertError.name,
+        payload: [`Failed to approve ${props.name}`],
+      });
+    } finally {
+      setIsLoading(false);
+      props.onClose();
+    }
+  };
+
+  return (
+    <ConfirmModal
+      isOpen={props.isOpen}
+      title="Approve certificate signing request"
+      body={`Are you sure you want to approve ${props.name}?`}
+      description={
+        <div className={styles.body}>
+          <Stack direction="column">
+            {isLoading && (
+              <LoadingPlaceholder text="Approving certificate signing request..." />
+            )}
+          </Stack>
+        </div>
+      }
+      confirmText="Approve"
+      confirmButtonVariant="primary"
+      dismissText="Cancel"
+      onConfirm={onConfirm}
+      onDismiss={() => props.onClose()}
+      disabled={isLoading}
+    />
+  );
+}
+
+const getStyles = (theme: GrafanaTheme2) => {
+  return {
+    body: css({
+      marginTop: theme.spacing(2),
+    }),
+  };
+};
